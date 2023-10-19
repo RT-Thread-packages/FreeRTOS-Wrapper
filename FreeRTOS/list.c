304e5cbdd15c783300c511a02971c72a6f9ae93f
wcx1024979076 <timwcx@qq.com>
2023-10-19 21:06:08 +0800

    更改 list.c include路径问题  & 完善文档

--- FreeRTOS/list.c
+++ FreeRTOS/list.c
@@ -35,7 +35,7 @@
 #define MPU_WRAPPERS_INCLUDED_FROM_API_FILE
 
 #include "FreeRTOS.h"
-#include "list.h"
+#include "freertos/list.h"
 
 /* Lint e9021, e961 and e750 are suppressed as a MISRA exception justified
  * because the MPU ports require MPU_WRAPPERS_INCLUDED_FROM_API_FILE to be
